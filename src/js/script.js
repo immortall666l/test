d00201ad332c2f613a819d553faa1dd76d335ca8
Dmitry <immortall666l@mail.ru>
2019-02-19 22:09:48 +0300

    комит второго домашнего задания

--- src/js/script.js
+++ src/js/script.js
@@ -33,16 +33,122 @@ window.addEventListener('DOMContentLoaded', () => {
             
             trigger.remove();
 
+            showConfirm();
+            calcGoods(1);
+            
             removeBtn.classList.add('goods__item-remove');
             removeBtn.innerHTML = '&times';
             item.appendChild(removeBtn);    
 
             cartWrapper.appendChild(item);
             if (empty) {
-                empty.remove();
+                empty.style.display = 'none';
             }
+
+            calcTotal();
+            removeFromCart();
+
         });
     });
 
+
+    function sliceTitle() {
+        titles.forEach(function(item) {
+            if (item.textContent.length < 70) {
+                return;
+            } else {
+                const str = item.textContent.slice(0, 71) + '...';
+                //const str = `${item.textContent.slice(0, 71)} ...`; ES6
+                item.textContent = str;
+            }
+        });
+    }
+    sliceTitle();
+
+    function showConfirm() {
+        confirm.style.display = 'block';
+        let counter = 100;
+        const id = setInterval (frame, 10);
+        function frame() {
+            if (counter == 10) {
+                clearInterval(id);
+                confirm.style.display = 'none';
+            } else {
+                counter--;  //counter = counter -1; 
+                confirm.style.transform = `translateY(-${counter}px)`; //блок confirm будет сдвгаться по оси вверх (-)
+                confirm.style.opacity = '.' + counter; //анимация будет плавно становиться прозрачной (opacity)
+            }
+        }
+    }
+    //setInterval(sliceTitle, 30) - запускает скрипт каждый отрезок времени (каждые 30 сек, напримерs)
+    //setTimeout(sliceTitle, 30) - запускает скрип через определенное время (30 сек, например)
+
+    function calcGoods(i) {
+        const items = cartWrapper.querySelectorAll('.goods__item');
+        let empty = cartWrapper.querySelector('.empty');
+        badge.textContent = i + items.length; //считает к-во элементов в корзине
+        if (badge.textContent < 1) {
+            empty.style.display = 'block'; //Домашнее задание 2!!!
+        }
+        
+    }
+
+    function calcTotal() {
+        const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+        let total = 0; 
+        prices.forEach(function(item) {
+            total += +item.textContent; //+= сокращенная форма записи чтобы добавлять значения, +item.textContent (+)  - делает из строчного элемента числовой
+        });
+        totalCost.textContent = total;
+        
+    }
+
+    function removeFromCart() {
+        const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+        removeBtn.forEach(function(btn) {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                calcGoods(0); 
+                calcTotal();
+               
+            });
+        });
+    }
 });
 
+
+
+
+
+
+
+
+
+
+
+
+//переменные - var, let, const;
+
+//var - переменную можно использовать только когда она объявлена
+    //(hosting) -  
+//    console.log(a); //переменная еще пустая (undefined), но переменная будет существовать до того как она объявлена. т.к. интерпретатор объявляет все переменные изначально
+//    var a = 5; // в переменную помещено число 5
+//    console.log(a); //выводит значение переменной - 5
+
+//let - переменную можно использовать только когда она объявлена
+//    console.log(a); //переменная не существует, тип данных (is not defined)
+//    let a = 5; // в переменную помещено число 5
+ //   console.log(a); //выводит значение переменной - 5
+
+//const - желательно использовать, но переменная которая 100% будет меняться указывать через let
+//    const a = 5; // в переменную помещено число 5
+//    console.log(a); //выводит значение переменной - 5
+//    a = 10; //невозможно перезаписать константу. (let и var можно перезаписать)
+//    console.log(a); //
+
+
+ //   const a = {
+ //       name: 'Alex' 
+ //   };
+//    a.name = 'Ivan'; //const можно поменять. Мутирует объект (посмотреть иммутабельность)
+//    console.log(a); 
